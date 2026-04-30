5964c7937c595e6a7c67e6a4ef5acc83f6ccee5a
Hancouree <sidormiks567@gmail.com>
2026-04-30 16:45:14 +0300

    feat: add new handlers for pub/sub commands

---
+++ src/SessionHandler.cpp
@@ -0,0 +1,137 @@
+#include "../include/SessionHandler.h"
+#include "../include/Utils.h"
+#include "../include/Session.h"
+
+std::string SubscribeHandler::execute(
+    const std::vector<std::string>& args, 
+    std::shared_ptr<ServerContext>& serverCtx, 
+    Session* session
+) {
+    if (args.size() < 2) {
+        return Utils::Resp::error("wrong number of arguments for SUBSCRIBE");
+    }
+
+    std::vector<std::string> channelsToSubscribe = { args.begin() + 1, args.end() };
+    std::string response = "";
+    for (const auto& c : channelsToSubscribe) {
+        serverCtx->m_pubSubRepo->subscribe(c, session);
+        session->addChannel(c);
+
+        response += "*3\r\n" +
+                    Utils::Resp::bulk("subscribe") +
+                    Utils::Resp::bulk(c) +
+                    Utils::Resp::integer(session->subscribedChannels());
+    }
+
+    return response;
+}
+
+std::string PublishHandler::execute(
+    const std::vector<std::string>& args, 
+    std::shared_ptr<ServerContext>& serverCtx, 
+    Session* session
+) {
+    if (args.size() < 3) {
+        return Utils::Resp::error("wrong number of arguments for PUBLISH");
+    }
+
+    std::string channel = args[1];
+    int count = serverCtx->m_pubSubRepo->publish(channel, args[2]);
+    return Utils::Resp::integer(count);
+}
+
+std::string UnsubscribeHandler::execute(
+    const std::vector<std::string>& args,
+    std::shared_ptr<ServerContext>& serverCtx,
+    Session* session
+) {
+    std::vector<std::string> channelsToRemove;
+
+    if (args.size() < 2) {
+        const auto& channels = session->getSubscribedChannels();
+        channelsToRemove.assign(channels.begin(), channels.end());
+    }
+    else {
+        channelsToRemove.assign(args.begin() + 1, args.end());
+    }
+
+    std::string response = "";
+    for (const auto& channel : channelsToRemove) {
+        serverCtx->m_pubSubRepo->unsubscribe(channel, session);
+        session->removeChannel(channel);
+
+        response += "*3\r\n" +
+                    Utils::Resp::bulk("unsubscribe") +
+                    Utils::Resp::bulk(channel) +
+                    Utils::Resp::integer(session->subscribedChannels());
+    }
+
+    if (response.empty()) {
+        response = "*3\r\n" +
+                    Utils::Resp::bulk("unsubscribe") +
+                    Utils::Resp::nil() +
+                    Utils::Resp::integer(0);
+    }
+
+    return response;
+}
+
+std::string PSubscribeHandler::execute(
+    const std::vector<std::string>& args, 
+    std::shared_ptr<ServerContext>& serverCtx, 
+    Session* session
+) {
+    if (args.size() < 2) {
+        return Utils::Resp::error("wrong number of arguments for PSUBSCRIBE");
+    }
+
+    std::vector<std::string> patternsToSubscribe = { args.begin() + 1, args.end() };
+    std::string response = "";
+    for (const auto& p : patternsToSubscribe) {
+        serverCtx->m_pubSubRepo->psubscribe(p, session);
+        session->addPattern(p);
+
+        response += "*3\r\n" +
+            Utils::Resp::bulk("psubscribe") +
+            Utils::Resp::bulk(p) +
+            Utils::Resp::integer(session->subscribedPatterns());
+    }
+
+    return response;
+}
+
+std::string PUnsubscribeHandler::execute(
+    const std::vector<std::string>& args, 
+    std::shared_ptr<ServerContext>& serverCtx, 
+    Session* session
+) {
+    std::vector<std::string> patternsToRemove;
+
+    if (args.size() < 2) {
+        const auto& patterns = session->getSubscribedPatterns();
+        patternsToRemove.assign(patterns.begin(), patterns.end());
+    }
+    else {
+        patternsToRemove.assign(args.begin() + 1, args.end());
+    }
+
+    std::string response = "";
+    for (const auto& pattern : patternsToRemove) {
+        serverCtx->m_pubSubRepo->punsubscribe(pattern, session);
+        session->removePattern(pattern);
+
+        response += "*3\r\n" +
+            Utils::Resp::bulk("punsubscribe") +
+            Utils::Resp::bulk(pattern) +
+            Utils::Resp::integer(session->subscribedChannels());
+    }
+
+    if (response.empty()) {
+        response = "*3\r\n" +
+            Utils::Resp::bulk("punsubscribe") +
+            Utils::Resp::nil() +
+            Utils::Resp::integer(0);
+    }
+
+    return response;
+}
