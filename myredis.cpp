3b17e2024d3a0e14eb531fd7850e59f1c6380583
Hancouree <sidormiks567@gmail.com>
2026-05-07 16:54:37 +0300

    feat: implement config

--- myredis.cpp
+++ myredis.cpp
@@ -3,10 +3,13 @@
 #include "include/Listener.h"
 #include "include/Cleaner.h"
 #include "include/CommandDocs.h"
+#include "include/Config.h"
 
 int main()
 {
-    auto serverCtx = std::make_shared<ServerContext>();
+    auto cfg = std::make_shared<Config>("myredis.conf");
+    auto serverCtx = std::make_shared<ServerContext>(cfg);
+    
     Registry::init();
     CommandDocs::init();
 
